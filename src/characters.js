464ced2085284ccdb7e62bdaf79e1d4f8ccdedb1
Tauã Mendonça <63937691+tauamendonca@users.noreply.github.com>
2025-08-03 13:31:47 -0300

    Separação da lógica de corrida e personagens do arquivo principal

--- src/characters.js
+++ src/characters.js
@@ -1,49 +1,50 @@
-const char1 = {
-  NOME: "Mario",
-  VELOCIDADE: 4,
-  MANOBRABILIDADE: 3,
-  PODER: 3,
-  PONTOS: 0,
-};
-
-const char2 = {
-  NOME: "Luigi",
-  VELOCIDADE: 3,
-  MANOBRABILIDADE: 4,
-  PODER: 4,
-  PONTOS: 0,
-};
-
-const char3 = {
-  NOME: "Peach",
-  VELOCIDADE: 3,
-  MANOBRABILIDADE: 4,
-  PODER: 2,
-  PONTOS: 0,
-};
-
-const char4 = {
-  NOME: "Yoshi",
-  VELOCIDADE: 2,
-  MANOBRABILIDADE: 4,
-  PODER: 3,
-  PONTOS: 0,
-};
-
-const char5 = {
-  NOME: "Bowser",
-  VELOCIDADE: 5,
-  MANOBRABILIDADE: 2,
-  PODER: 5,
-  PONTOS: 0,
-};
-
-const char6 = {
-  NOME: "Donkey Kong",
-  VELOCIDADE: 2,
-  MANOBRABILIDADE: 2,
-  PODER: 5,
-  PONTOS: 0,
-};
-
-module.exports = {char1, char2, char3, char4, char5, char6};
+export const characters = [
+    {
+        ID: 1,
+        NOME: "Mario",
+        VELOCIDADE: 4,
+        MANOBRABILIDADE: 3,
+        PODER: 3,
+        PONTOS: 0,
+    },
+    {
+        ID: 2,
+        NOME: "Luigi",
+        VELOCIDADE: 3,
+        MANOBRABILIDADE: 4,
+        PODER: 4,
+        PONTOS: 0,
+    },
+    {
+        ID: 3,
+        NOME: "Peach",
+        VELOCIDADE: 3,
+        MANOBRABILIDADE: 4,
+        PODER: 2,
+        PONTOS: 0,
+    },
+    {
+        ID: 4,
+        NOME: "Yoshi",
+        VELOCIDADE: 2,
+        MANOBRABILIDADE: 4,
+        PODER: 3,
+        PONTOS: 0,
+    },
+    {
+        ID: 5,
+        NOME: "Bowser",
+        VELOCIDADE: 5,
+        MANOBRABILIDADE: 2,
+        PODER: 5,
+        PONTOS: 0,
+    },
+    {
+        ID: 6,
+        NOME: "Donkey Kong",
+        VELOCIDADE: 2,
+        MANOBRABILIDADE: 2,
+        PODER: 5,
+        PONTOS: 0,
+    },
+];
